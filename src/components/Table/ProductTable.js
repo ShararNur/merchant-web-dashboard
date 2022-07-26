898ba230896257f213f416118e3741b082418223
iamjnayem <iamj.nayem@gmail.com>
2022-07-26 18:26:05 +0600

    Product Table Component

---
+++ src/components/Table/ProductTable.js
@@ -0,0 +1,62 @@
+import productIcon from '../../assets/images/image 65.png';
+import actionIcon from '../../assets/icons/Ellipse 1.png';
+import './style.product_table.css';
+const data = [
+    {name: 'Miniket Rice Premium', category: 'Groceries', unit: '5 kg Pack', stock: 5, price: '৳ 305.00'},
+    {name: 'Miniket Rice Premium', category: 'Groceries', unit: '5 kg Pack', stock: 5, price: '৳ 305.00'},
+    {name: 'Miniket Rice Premium', category: 'Groceries', unit: '5 kg Pack', stock: 5, price: '৳ 305.00'},
+    {name: 'Miniket Rice Premium', category: 'Groceries', unit: '5 kg Pack', stock: 5, price: '৳ 305.00'},
+    {name: 'Miniket Rice Premium', category: 'Groceries', unit: '5 kg Pack', stock: 5, price: '৳ 305.00'},
+    {name: 'Miniket Rice Premium', category: 'Groceries', unit: '5 kg Pack', stock: 5, price: '৳ 305.00'},
+    {name: 'Miniket Rice Premium', category: 'Groceries', unit: '5 kg Pack', stock: 5, price: '৳ 305.00'},
+    {name: 'Miniket Rice Premium', category: 'Groceries', unit: '5 kg Pack', stock: 5, price: '৳ 305.00'},
+    {name: 'Miniket Rice Premium', category: 'Groceries', unit: '5 kg Pack', stock: 5, price: '৳ 305.00'},
+    {name: 'Miniket Rice Premium', category: 'Groceries', unit: '5 kg Pack', stock: 5, price: '৳ 305.00'},
+    {name: 'Miniket Rice Premium', category: 'Groceries', unit: '5 kg Pack', stock: 5, price: '৳ 305.00'},
+    {name: 'Miniket Rice Premium', category: 'Groceries', unit: '5 kg Pack', stock: 5, price: '৳ 305.00'},
+    {name: 'Miniket Rice Premium', category: 'Groceries', unit: '5 kg Pack', stock: 5, price: '৳ 305.00'},
+    {name: 'Miniket Rice Premium', category: 'Groceries', unit: '5 kg Pack', stock: 5, price: '৳ 305.00'},
+    {name: 'Miniket Rice Premium', category: 'Groceries', unit: '5 kg Pack', stock: 5, price: '৳ 305.00'},
+    {name: 'Miniket Rice Premium', category: 'Groceries', unit: '5 kg Pack', stock: 5, price: '৳ 305.00'},
+    {name: 'Miniket Rice Premium', category: 'Groceries', unit: '5 kg Pack', stock: 5, price: '৳ 305.00'},
+    {name: 'Miniket Rice Premium', category: 'Groceries', unit: '5 kg Pack', stock: 5, price: '৳ 305.00'},
+
+]
+
+const productTable = () => {
+    return (
+        <table className="table table-auto min-w-full text-left">
+            <tr >
+                <th className="first-heading">Item Image</th>
+                <th>Item Name</th>
+                <th>Category</th>
+                <th>Unit</th>
+                <th>Stock</th>
+                <th>Price</th>
+            </tr>
+          {
+            data.map((val, key) => {
+                return(
+                    <tr key={key}>
+                        <td><img src={productIcon} alt="product icon" className="inline-block icon"/></td>
+                        <td>{val.name}</td>
+                        <td>{val.category}</td>
+                        <td>{val.unit}</td>
+                        <td>{val.stock}</td>
+                        <td>
+                            {val.price} 
+                            <span className='action-icons'>
+                                <img src={actionIcon} className="inline-block" alt="action icon"/>
+                                <img src={actionIcon} className="inline-block" alt="action icon"/>
+                                <img src={actionIcon} className="inline-block" alt="action icon"/>
+                            </span>
+                        </td>
+                    </tr>
+                )
+            })
+          }
+        </table>
+    );
+}
+
+export default productTable;
